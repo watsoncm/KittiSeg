2a0e554760c1c1adada9251592b2c1050574b413
Anjali <aroyc@stanford.edu>
2018-03-18 20:47:28 -0700

    added layer dropping logic..?

--- fast.py
+++ fast.py
@@ -14,12 +14,21 @@ import logging
 import os
 import sys
 import string
+import numpy as np
 
 import collections
 
 
 NUM_GENERATIONS = 10
 MEMBERS_PER_GEN = 5
+layerList = ['conv1_1', 'conv1_2', 'conv2_1', 'conv2_2', 'conv3_1', 'conv3_2',
+             'conv3_3', 'conv4_1', 'conv4_2', 'conv4_3', 'conv5_1', 'conv5_2',
+             'conv5_3', 'fc6', 'fc7']
+layerPrior = [0.1, 0.1, 0.2, 0.2, 0.3, 0.3,
+              0.4, 0.5, 0.4, 0.3, 0.3, 0.2,
+              0.2, 0.1, 0.1]
+
+fastDict = {'drop_layers': []}
 
 # configure logging
 if 'TV_IS_DEV' in os.environ and os.environ['TV_IS_DEV']:
@@ -109,18 +118,64 @@ def main(_):
     #######
 
     ga_content = {'encoder_name': 'fcn8_vgg',
-                  'encoder_path': hypes['model']['architecture_file']}
+                  'encoder_path': hypes['model']['architecture_file'], 'drop': []}
     hypes['model']['architecture_file'] = '../encoder/stub.py'
     hypes['ga_data'] = 'ga_data.json'
 
-    
-    with open(hypes['ga_data'], 'w') as f:
-        commentjson.dump(ga_content, f)
+# pass on to next generation??
 
+
+    prevTopDropLists = []
     for i in NUM_GENERATIONS:
-        pass
+        childrenRuntime = np.zeros(MEMBERS_PER_GEN)
+        childrenDropLists = []
+        prevListsCombined = combineDropLists(prevTopDropLists, len(layerList))
+        for j in range(MEMBERS_PER_GEN): # for each child
+            dropList = []
+            if j!=0 or j!=1: #first should be the control/empty droplist per generation, second is hybrid of above
+                for k in range(len(layerList)):
+                    if layerPrior[k] >= np.random.random():
+                        dropList.append(layerList[k])
+            if j==1:
+                dropList = prevListsCombined
+            with open(hypes['ga_data'], 'w') as f:
+                commentjson.dump(ga_content, f)
+            ga_content['drop'] = dropList
+            train.do_training(hypes)
+            childrenRuntime[j] = 5 #Chandler put actual runtime here pls
+            childrenDropLists.append(dropList)
+        # pull out top n children's droplists
+        sortedChildrenRuntimeIndices = np.argsort(childrenRuntime)
+        n = 2 # number of top lists we want
+        prevTopDropLists = []
+        for i in range(n):
+            # 0th should be last item, 1st should be second last, etc.
+            topNIndex = sortedChildrenRuntimeIndices(sortedChildrenRuntimeIndices.shape[0]-1-n)
+            prevTopDropLists.append(childrenDropLists[topNIndex])
 
-    train.do_training(hypes)
+
+def combineDropLists(Lists, length):
+    #Lists = a list of lists
+    if len(Lists) != 0:
+        list1 = Lists[0]
+        list2 = Lists[1]
+        combinedList = []
+        for i in range(length):
+            x = np.random.random()
+            if np.round(x) ==0:
+                combinedList.append(list1[i])
+            else:
+                combinedList.append(list2[i])
+        return combinedList
+    else:
+        return []
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
